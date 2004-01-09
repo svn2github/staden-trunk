40d5b6a072909b9df5de9127d421012129ca1e3c
mng <mng@ba6f2710-ca9f-427c-89d3-5bf716dbc8e1>
2004-01-09 14:25:21 +0000

    Contig navigation interface.
    git-svn-id: https://staden.svn.sourceforge.net/svnroot/staden/staden/trunk@179 ba6f2710-ca9f-427c-89d3-5bf716dbc8e1

---
+++ src/gap4/gap_nav.tcl
@@ -0,0 +1,229 @@
+
+proc ViewNavigationData {io} {
+
+    global gap_defs
+    
+    set w .nav
+    if {[xtoplevel $w] == ""} return
+    wm title $w "Contig Navigation File"
+    
+    xget_fname $w.navfile \
+        -text "Navigation file" \
+        -type text \
+
+    pack $w.navfile -side top -fill both
+
+    okcancelhelp $w.ok \
+        -ok_command "OpenNavFile $io $w" \
+        -cancel_command "destroy $w" \
+        -help_command "show_help gap4 {Contig Navigation}" \
+        -bd 2 -relief groove
+    
+    pack $w.ok -side top -fill both
+}
+
+
+proc OpenNavFile { io w } {
+
+    global $w.File $w.Auto $w.Edwin
+    
+    set fname [$w.navfile get2]
+    if {$fname == ""} {
+        bell
+        return
+    }
+    if {[catch {set fd [open $fname r]}]} {
+        bell
+        tk_messageBox -message "Couldn't open $fname" \
+            -type ok \
+            -parent $w
+        return
+    }
+    
+    set $w.File [split [read $fd] "\n"]
+    
+    close $fd
+    
+    CreateTableList $io $w
+     
+}
+
+proc CreateTableList { io w } {
+    package require Tablelist
+    
+    global gap_defs $w.File $w.Auto $w.Edwin 
+
+    set $w.Auto 1
+
+    set t .navtable
+    if {[xtoplevel $t] == ""} return
+    wm title $t "Navigate Regions"
+    
+    frame $t.f
+    
+    set tbl $t.f.tbl
+    
+    tablelist::tablelist $tbl \
+        -columns {12 "ContigID"  12 "Position"  0 "Problem Type"} \
+        -showseparators yes \
+        -background gray96 -selectbackground navy -selectforeground white \
+        -width 100 -stretch 2 \
+        -labelcommand tablelist::sortByColumn -sortcommand compareAsSet \
+        -yscrollcommand [list $t.f.yscroll set] \
+        -selectmode single \
+        -exportselection 0
+
+    $tbl columnconfigure 1 -sortmode integer
+    
+    
+    foreach row [set $w.File] {        
+        if {[regexp {^([^ ]*) +([^ ]*) *([^\x0]*)(\x0)?} $row dummy cid pos comm hit] == 0} {
+            continue
+        }
+        $tbl insert end \
+            [list "$cid" "$pos" "$comm"]        
+        if {$hit == "\0"} {
+            set bg [keylget gap_defs NAVIGATION.SELECT_COLOUR]
+            $tbl rowconfigure end -bg $bg
+        }
+    }
+
+    set body [$tbl bodypath]
+    bind $body <Double-1> [list RaiseAndMoveContig $io $tbl $w]
+
+    scrollbar $t.f.yscroll -command "$tbl yview"
+    pack $t.f.yscroll -side right -fill both
+    pack $t.f.tbl -side top -fill both -expand 1
+    
+    frame $t.b
+    pack $t.b -side bottom -expand yes -fill both -padx 5
+    
+    button $t.b.save -text "Save"    -command "SaveTable $w $tbl"
+    button $t.b.close -text "Close"  -command "destroy $t"
+    button $t.b.help -text "Help"    -command "show_help gap4 {Contig Navigation}"
+    button $t.b.reset -text "Reset"  -command "ResetTable $tbl $w"
+    button $t.b.prev -text "  <<-  " -command "PrevProblem $io $tbl $w" 
+    button $t.b.next -text "  ->>  " -command "NextProblem $io $tbl $w" 
+    checkbutton $t.b.auto -text "auto-close editors" \
+        -variable $w.Auto -offvalue 0 -onvalue 1
+        
+    pack $t.b.reset -side left  -padx 10
+    pack $t.b.prev -side left
+    pack $t.b.next -side left
+    pack $t.b.auto -side left   -padx 10
+    pack $t.b.help -side right  -padx 10
+    pack $t.b.close -side right -padx 10
+    pack $t.b.save -side right  -padx 10
+    
+    pack $t.f -side top -expand yes -fill both -padx 5 -pady 5
+}
+
+
+proc SaveTable { w tbl } {
+
+    global gap_defs
+    
+    set fname [$w.navfile get2]
+    if {$fname == ""} {
+        bell
+        return
+    }
+    if {[catch {set fd [open $fname w]}]} {
+        bell
+        tk_messageBox -message "Couldn't open $fname for writing." 
+        -type ok 
+        -parent $w
+        return
+    }
+    
+    set rowCount [$tbl size]
+    for {set row 0} {$row < $rowCount} {incr row} {
+	set ct [$tbl cellcget $row,0 -text]
+        set po [$tbl cellcget $row,1 -text]
+        set cm [$tbl cellcget $row,2 -text]        
+        set col [$tbl rowcget $row -bg] 
+
+        if {[$tbl rowcget $row -bg] == [keylget gap_defs NAVIGATION.SELECT_COLOUR]} {
+            puts $fd "$ct $po $cm \0"
+        } else {
+            puts $fd "$ct $po $cm" 
+        }
+    }       
+    close $fd  
+}
+
+proc PrevProblem { io tbl w } {
+    set sel [$tbl curselection] 
+    set pn [expr {$sel-1}]
+    if {[expr {$pn < 0}]} {
+        bell
+        return
+    }
+    $tbl selection clear $sel
+    $tbl selection set $pn
+    $tbl see $pn
+    RaiseAndMoveContig $io $tbl $w
+}
+
+proc NextProblem { io tbl w } {
+    set sel [$tbl curselection]
+    set pn [expr {$sel+1}]
+    if {[expr {$pn == [$tbl size]}]} {
+        bell
+        return
+    }
+    $tbl selection clear $sel
+    $tbl selection set $pn
+    $tbl see $pn
+    RaiseAndMoveContig $io $tbl $w
+}
+
+proc ResetTable { tbl w } {
+    global $w.File 
+
+    $tbl delete 0 end
+
+    foreach row [set $w.File] {        
+        if {[regexp {^([^ ]*) +([^ ]*) *([^\x0]*)} $row dummy cid pos comm] == 0} {
+            continue
+        }
+        $tbl insert end \
+            [list "$cid" "$pos" "$comm"]        
+    }     
+}
+
+proc RaiseAndMoveContig {io tbl w} {
+    
+    global gap_defs $w.Edwin $w.Auto 
+
+    set sel [$tbl curselection]
+    set ctg [$tbl cellcget [$tbl curselection],0 -text]
+    set pos [$tbl cellcget [$tbl curselection],1 -text]
+    
+    set ed [edit_contig -io $io -contig $ctg -pos $pos -reuse 1 -nojoin 1]
+
+    if {[expr {[set $w.Auto] == 1}]} {
+        if {[info exists $w.Edwin] && [expr {[set $w.Edwin] != $ed}]} {
+            catch { destroy [winfo toplevel [set $w.Edwin]] }
+        }
+        set $w.Edwin $ed
+    }
+    set bg [keylget gap_defs NAVIGATION.SELECT_COLOUR]
+    $tbl rowconfigure $sel -bg $bg
+    
+}
+
+proc compareAsSet {item1 item2} {
+    foreach {opt1 dbName1 dbClass1 default1 current1} $item1 \
+        {opt2 dbName2 dbClass2 default2 current2} $item2 {
+            set changed1 [expr {[string compare $default1 $current1] != 0}]
+            set changed2 [expr {[string compare $default2 $current2] != 0}]
+            if {$changed1 == $changed2} {
+                return [string compare $opt1 $opt2]
+            } elseif {$changed1} {
+                return -1
+            } else {
+                return 1
+            }
+        }
+}
