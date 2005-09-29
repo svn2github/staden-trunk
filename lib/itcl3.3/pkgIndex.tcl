817ed703ec13d66961d7ee6ee0bf477524856e5f
jkbonfield <jkbonfield@ba6f2710-ca9f-427c-89d3-5bf716dbc8e1>
2005-09-29 10:58:02 +0000

    Auto-detect windows vs unix dll name.
    git-svn-id: https://staden.svn.sourceforge.net/svnroot/staden/staden/trunk@840 ba6f2710-ca9f-427c-89d3-5bf716dbc8e1

--- lib/itcl3.3/pkgIndex.tcl
+++ lib/itcl3.3/pkgIndex.tcl
@@ -1,3 +1,8 @@
 # Tcl package index file, version 1.0
 
-package ifneeded Itcl 3.3 [list load [file join $dir "libitcl3.3.so"] Itcl]
+if {$tcl_platform(platform) == "windows"} {
+    package ifneeded Itcl 3.3 [list load [file join $dir "itcl33.dll"] Itcl]
+} else {
+    package ifneeded Itcl 3.3 [list load [file join $dir "libitcl3.3.so"] Itcl]
+}
+
--- lib/itk3.3/pkgIndex.tcl
+++ lib/itk3.3/pkgIndex.tcl
@@ -1,3 +1,7 @@
 # Tcl package index file, version 1.0
 
-package ifneeded Itk 3.3 [list load [file join $dir "libitk3.3.so"] Itk]
+if {$tcl_platform(platform) == "windows"} {
+    package ifneeded Itk 3.3 [list load [file join $dir "itk33.dll"] Itk]
+} else {
+    package ifneeded Itk 3.3 [list load [file join $dir "libitk3.3.so"] Itk]
+}
