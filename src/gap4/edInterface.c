7df000eb984d54ac2b175b3c2265b9fb966f0063
jkbonfield <jkbonfield@ba6f2710-ca9f-427c-89d3-5bf716dbc8e1>
2006-08-11 11:23:25 +0000

    Fixed buffer underrun in edNextDifference.
    git-svn-id: https://staden.svn.sourceforge.net/svnroot/staden/staden/trunk@1041 ba6f2710-ca9f-427c-89d3-5bf716dbc8e1

--- src/gap4/edInterface.c
+++ src/gap4/edInterface.c
@@ -2761,7 +2761,7 @@ void edNextDifference(EdStruct *xx, int fwd) {
 		break;
 	}
     } else {
-	while (--pos1 >= 0 && --pos2 >= 0) {
+	while (--pos1 > 0 && --pos2 > 0) {
 	    if (con1[pos1-1] != con2[pos2-1])
 		break;
 	}
