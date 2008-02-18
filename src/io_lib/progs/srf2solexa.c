26ab888faf79ff454234a574d198046b4d9dd861
jkbonfield <jkbonfield@ba6f2710-ca9f-427c-89d3-5bf716dbc8e1>
2008-02-18 16:56:39 +0000

    Major overhaul to support raw data as well as processed data. Still to-do: write out .params and the two matrix files.
    git-svn-id: https://staden.svn.sourceforge.net/svnroot/staden/staden/trunk@1183 ba6f2710-ca9f-427c-89d3-5bf716dbc8e1

--- src/io_lib/progs/srf2solexa.c
+++ src/io_lib/progs/srf2solexa.c
@@ -38,105 +38,327 @@
 #include <io_lib/ztr.h>
 #include <io_lib/srf.h>
 
+#define RAW       (1<<0)
+#define PROCESSED (1<<1)
+
 /* ------------------------------------------------------------------------ */
+
+/*
+ * Parses a name assuming it consists of:
+ * prefix<separator><lane><separator><tile><separator><x><separator><y>
+ *
+ * We fill out the supplied lane, tile, x and y parameters, or set them to
+ * 0, 0, 0 and 0 if unknown.
+ *
+ * Returns 0 for success
+ *        -1 for failure (unknown)
+ */
+int parse_name(char *name, int *lane, int *tile, int *x, int *y) {
+    size_t len = strlen(name), i;
+
+    /* FIXME: lack of error checking */
+
+    i = len-1;
+    while (i >= 0 && isdigit(name[i]))
+	i--;
+    *y = atoi(&name[i+1]);
+    i--;
+    
+    while (i >= 0 && isdigit(name[i]))
+	i--;
+    *x = atoi(&name[i+1]);
+    i--;
+
+    while (i >= 0 && isdigit(name[i]))
+	i--;
+    *tile = atoi(&name[i+1]);
+    i--;
+
+    while (i >= 0 && isdigit(name[i]))
+	i--;
+    *lane = atoi(&name[i+1]);
+    i--;
+
+    return 0;
+}
+
+#define MAX_CYCLES 500
+void dump_samples4(FILE *fp, int lane, int tile, int x, int y,
+		   int baseline, unsigned char *bytes, int nbytes) {
+    int i, j, nb;
+    int A[MAX_CYCLES], C[MAX_CYCLES], G[MAX_CYCLES], T[MAX_CYCLES];
+
+    nb = nbytes/8;
+
+    for (i = j = 0; i < nb; i++, j+= 2)
+	A[i] = (bytes[j] << 8) | bytes[j+1];
+
+    for (i = 0; i < nb; i++, j+= 2)
+	C[i] = (bytes[j] << 8) | bytes[j+1];
+
+    for (i = 0; i < nb; i++, j+= 2)
+	G[i] = (bytes[j] << 8) | bytes[j+1];
+
+    for (i = 0; i < nb; i++, j+= 2)
+	T[i] = (bytes[j] << 8) | bytes[j+1];
+
+    fprintf(fp, "%d\t%d\t%d\t%d", lane, tile, x, y);
+    for (i = 0; i < nb; i++)
+	fprintf(fp, "\t%d %d %d %d",
+		A[i] - baseline,
+		C[i] - baseline,
+		G[i] - baseline,
+		T[i] - baseline);
+    fprintf(fp, "\n");
+}
+
+void dump_conf4(FILE *fp, char *seq, signed char *bytes, int nbytes) {
+    int i, j, nb;
+    int A[MAX_CYCLES], C[MAX_CYCLES], G[MAX_CYCLES], T[MAX_CYCLES];
+
+    nb = nbytes/4;
+
+    for (i = 0, j = nb; i < nb; i++) {
+	switch(seq[i]) {
+	case 'A': case 'a':
+	    A[i] = bytes[i];
+	    C[i] = bytes[j++];
+	    G[i] = bytes[j++];
+	    T[i] = bytes[j++];
+	    break;
+	case 'C': case 'c':
+	    A[i] = bytes[j++];
+	    C[i] = bytes[i];
+	    G[i] = bytes[j++];
+	    T[i] = bytes[j++];
+	    break;
+	case 'G': case 'g':
+	    A[i] = bytes[j++];
+	    C[i] = bytes[j++];
+	    G[i] = bytes[i];
+	    T[i] = bytes[j++];
+	    break;
+	default:
+	    A[i] = bytes[j++];
+	    C[i] = bytes[j++];
+	    G[i] = bytes[j++];
+	    T[i] = bytes[i];
+	    break;
+	}
+    }
+
+    for (i = 0; i < nb; i++) {
+	if (i)
+	    fputc('\t', fp);
+	fprintf(fp, "%4d %4d %4d %4d", A[i], C[i], G[i], T[i]);
+    }
+
+    fprintf(fp, "\n");
+}
 
 /*
  * Ripped out of io_lib's trace_dump program.
  * It reformats a trace to as printable ASCII.
  */
-void dump(ztr_t *z, char *name, FILE *fp_seq, FILE *fp_sig2, FILE *fp_prb) {
-    Read *read;
-    int i;
-    int lane=1, tile=1, x=1, y=1;
+void dump(ztr_t *z,
+	  int lane, int tile, int x, int y,
+	  FILE *fp_int, FILE *fp_nse,
+	  FILE *fp_seq, FILE *fp_sig2, FILE *fp_prb) {
+    int i, nc;
+    ztr_chunk_t **chunks;
+    char *seq;
 
     uncompress_ztr(z);
-    read = ztr2read(z); /* Inefficient; can do direct */
-
-    if (read == NULL) {
-	fprintf(stderr, "Tracedump was unable to open file %s\n", name );
-	return;
-    }
-
-    sscanf(name, "s_%d_%d_%d_%d", &lane, &tile, &x, &y);
 
     /* Sequence */
-    fprintf(fp_seq, "%d\t%d\t%d\t%d\t%.*s\n",
-	    lane, tile, x, y, read->NBases, read->base);
+    if (fp_seq || fp_prb) {
+	chunks = ztr_find_chunks(z, ZTR_TYPE_BASE, &nc);
+	if (nc != 1) {
+	    fprintf(stderr, "Zero or greater than one BASE chunks found.\n");
+	    return;
+	}
+	seq = chunks[0]->data+1;
 
-    /* Traces */
-    fprintf(fp_sig2, "%d\t%d\t%d\t%d", lane, tile, x, y);
-    for (i = 0; i < read->NBases; i++) {
-	fprintf(fp_sig2, "\t%d %d %d %d",
-		(int)read->traceA[i] - read->baseline,
-		(int)read->traceC[i] - read->baseline,
-		(int)read->traceG[i] - read->baseline,
-		(int)read->traceT[i] - read->baseline);
+	if (fp_seq)
+	    fprintf(fp_seq, "%d\t%d\t%d\t%d\t%.*s\n",
+		    lane, tile, x, y,
+		    chunks[0]->dlength-1,
+		    chunks[0]->data+1);
     }
-    fprintf(fp_sig2, "\n");
 
     /* Confidence */
-    for (i = 0; i < read->NBases; i++) {
-	if (i)
-	    fputc('\t', fp_prb);
-	fprintf(fp_prb, "%4d %4d %4d %4d",
-		(int)read->prob_A[i],
-		(int)read->prob_C[i],
-		(int)read->prob_G[i],
-		(int)read->prob_T[i]);
-    }
-    fprintf(fp_prb, "\n");
+    if (fp_prb) {
+	chunks = ztr_find_chunks(z, ZTR_TYPE_CNF4, &nc);
+	if (nc != 1) {
+	    fprintf(stderr, "Zero or greater than one CNF chunks found.\n");
+	    return;
+	}
 
-    read_deallocate(read);
+	dump_conf4(fp_prb, seq, chunks[0]->data+1, chunks[0]->dlength-1);
+    }
+
+    /* Traces */
+    if (fp_sig2) {
+	chunks = ztr_find_chunks(z, ZTR_TYPE_SMP4, &nc);
+	for (i = 0; i < nc; i++) {
+	    int baseline = 0;
+	    char *key = ztr_lookup_mdata_value(z, chunks[i], "TYPE");
+	    if (!key || 0 == strcmp(key, "PROC")) {
+		key = ztr_lookup_mdata_value(z, chunks[i], "OFFS");
+		dump_samples4(fp_sig2, lane, tile, x, y, key ? atoi(key) : 0,
+			      chunks[i]->data+2, chunks[i]->dlength-2);
+		break;
+	    }
+	}
+    }
+
+    if (fp_int) {
+	chunks = ztr_find_chunks(z, ZTR_TYPE_SMP4, &nc);
+	for (i = 0; i < nc; i++) {
+	    int baseline = 0;
+	    char *key = ztr_lookup_mdata_value(z, chunks[i], "TYPE");
+	    if (key && 0 == strcmp(key, "SLXI")) {
+		key = ztr_lookup_mdata_value(z, chunks[i], "OFFS");
+		dump_samples4(fp_int, lane, tile, x, y, key ? atoi(key) : 0,
+			      chunks[i]->data+2, chunks[i]->dlength-2);
+		break;
+	    }
+	}
+    }
+
+    if (fp_nse) {
+	chunks = ztr_find_chunks(z, ZTR_TYPE_SMP4, &nc);
+	for (i = 0; i < nc; i++) {
+	    int baseline = 0;
+	    char *key = ztr_lookup_mdata_value(z, chunks[i], "TYPE");
+	    if (key && 0 == strcmp(key, "SLXN")) {
+		key = ztr_lookup_mdata_value(z, chunks[i], "OFFS");
+		dump_samples4(fp_nse, lane, tile, x, y, key ? atoi(key) : 0,
+			      chunks[i]->data+2, chunks[i]->dlength-2);
+		break;
+	    }
+	}
+    }
 }
 
-/* ------------------------------------------------------------------------ */
-int main(int argc, char **argv) {
-    char *ar_name;
-    ztr_t *ztr;
-    srf_t *srf;
-    char name[512];
-    char *prefix = "s_lane_tile_";
-    FILE *fp_seq, *fp_sig2, *fp_prb;
 
-    if (argc != 2 && argc != 3) {
-	fprintf(stderr, "Usage: srf_dump archive_name [output_prefix]\n");
+/* ------------------------------------------------------------------------ */
+FILE *fopen_slx(char *dir, char *type, int lane, int tile) {
+    char fn[1024];
+    FILE *fp;
+
+    sprintf(fn, "%s/s_%d_%04d_%s.txt", dir, lane, tile, type);
+    if (NULL == (fp = fopen(fn, "w+"))) {
+	perror(fn);
+	return NULL;
+    }
+
+    return fp;
+}
+
+int process_srf(char *file, char *dir, int mode) {
+    srf_t *srf;
+    char name[1024], dir2[1024];
+    ztr_t *ztr;
+    int last_lane = 0, last_tile = 0;
+    FILE *fp_int = NULL;
+    FILE *fp_nse = NULL;
+    FILE *fp_seq = NULL;
+    FILE *fp_prb = NULL;
+    FILE *fp_sig = NULL;
+
+    if (NULL == (srf = srf_open(file, "r"))) {
+	perror(file);
 	return 1;
     }
-    ar_name = argv[1];
-    if (argc == 3)
-	prefix = argv[2];
 
-    if (NULL == (srf = srf_open(ar_name, "r"))) {
-	perror(ar_name);
-	return 4;
+    if (dir) {
+	strcpy(dir2, dir);
+    } else {
+	char *cp = strrchr(file, '.');
+	if (cp) *cp = 0;
+	sprintf(dir2, "%s.run", file);
     }
-
-    sprintf(name, "%sseq.txt", prefix);
-    if (NULL == (fp_seq = fopen(name, "w"))) {
-	perror(name);
-	return 5;
-    }
-    sprintf(name, "%sprb.txt", prefix);
-    if (NULL == (fp_prb = fopen(name, "w"))) {
-	perror(name);
-	return 5;
-    }
-    sprintf(name, "%ssig2.txt", prefix);
-    if (NULL == (fp_sig2 = fopen(name, "w"))) {
-	perror(name);
-	return 5;
-    }
+    mkdir(dir2, 0777);
 
     while (NULL != (ztr = srf_next_ztr(srf, name))) {
-	dump(ztr, name, fp_seq, fp_sig2, fp_prb);
+	int lane, tile, x, y;
+	parse_name(name, &lane, &tile, &x, &y);
+
+	if (last_lane != lane || last_tile != tile) {
+	    fprintf(stderr, "New tile: %d/%d\n", lane, tile);
+	    last_lane = lane;
+	    last_tile = tile;
+
+	    if (fp_int) { fclose(fp_int); fp_int = NULL; }
+	    if (fp_nse) { fclose(fp_nse); fp_nse = NULL; }
+	    if (fp_seq) { fclose(fp_seq); fp_seq = NULL; }
+	    if (fp_prb) { fclose(fp_prb); fp_prb = NULL; }
+	    if (fp_sig) { fclose(fp_sig); fp_sig = NULL; }
+
+	    if (mode & RAW) {
+		fp_int = fopen_slx(dir2, "int", lane, tile);
+		fp_nse = fopen_slx(dir2, "nse", lane, tile);
+	    }
+	    if (mode & PROCESSED) {
+		fp_seq = fopen_slx(dir2, "seq", lane, tile);
+		fp_prb = fopen_slx(dir2, "prb", lane, tile);
+		fp_sig = fopen_slx(dir2, "sig2", lane, tile);
+	    }
+	}
+	dump(ztr, lane, tile, x, y, fp_int, fp_nse, fp_seq, fp_sig, fp_prb);
 	delete_ztr(ztr);
     }
 
-    fclose(fp_seq);
-    fclose(fp_sig2);
-    fclose(fp_prb);
+    if (fp_int) fclose(fp_int);
+    if (fp_nse) fclose(fp_nse);
+    if (fp_seq) fclose(fp_seq);
+    if (fp_prb) fclose(fp_prb);
+    if (fp_sig) fclose(fp_sig);
 
     srf_destroy(srf, 1);
+    return 0;
+}
+
+/* ------------------------------------------------------------------------ */
+void usage(void) {
+    printf("Usage: srf2solexa [options]\n\n");
+    printf(" Options:\n");
+    printf("    -r       Output raw (.int/.nse) data only\n");
+    printf("    -p       Output processed (.sig2/.seq/.prb) data only\n");
+    printf("    -d dir   Set the output directory to 'dir'\n");
+    exit(0);
+}
+
+int main(int argc, char **argv) {
+    int mode = RAW | PROCESSED;
+    char *dir = NULL;
+    int i;
+
+    /* Parse args */
+    for (i = 1; i < argc && argv[i][0] == '-'; i++) {
+	if (!strcmp(argv[i], "-")) {
+	    break;
+	} else if (!strcmp(argv[i], "-r")) {
+	    mode = RAW;
+	} else if (!strcmp(argv[i], "-p")) {
+	    mode = PROCESSED;
+	} else if (!strcmp(argv[i], "-d")) {
+	    dir = argv[++i];
+	} else {
+	    usage();
+	}
+    }
+
+    if (i == argc)
+	usage();
+
+    for (; i < argc; i++) {
+	if (process_srf(argv[i], dir, mode)) {
+	    perror(argv[i]);
+	    return 1;
+	}
+    }
     
     return 0;
 }
