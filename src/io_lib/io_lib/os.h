0f485339174fc065ccf18e3a6bb81bf325eb80cf
jkbonfield <jkbonfield@ba6f2710-ca9f-427c-89d3-5bf716dbc8e1>
2008-09-11 08:44:56 +0000

    Tidy up of endianness detection. I split apart the endian step from the os-components (no strdup, etc). Also changed the order so that when using autoconf the automatically detected settings override any existing assumptions from os.h.
    git-svn-id: https://staden.svn.sourceforge.net/svnroot/staden/staden/trunk@1297 ba6f2710-ca9f-427c-89d3-5bf716dbc8e1

--- src/io_lib/io_lib/os.h
+++ src/io_lib/io_lib/os.h
@@ -30,13 +30,6 @@
 #define IOLIB_SFF
 #endif
 
-/* For when built as part of autoconf */
-#ifdef WORDS_BIGENDIAN
-#    define SP_BIG_ENDIAN
-#else
-#    define SP_LITTLE_ENDIAN
-#endif
-
 #include <limits.h>
 #include <inttypes.h>
 
@@ -72,7 +65,7 @@ extern "C" {
 
 /*
  *-----------------------------------------------------------------------------
- * Machine specifics
+ * Operating system specifics
  *-----------------------------------------------------------------------------
  */
 /*
@@ -86,7 +79,6 @@ extern "C" {
 #define SEEK_END 2
 #define FOPEN_MAX 64
 #define FILENAME_MAX 1024
-#define SP_BIG_ENDIAN
 
 /* Missing functions, defined in strings.c */
 #define NOMEMMOVE
@@ -98,11 +90,6 @@ extern "C" {
  * SunOS 5.x - gcc or Sun's cc 
  */ 
 #if (defined(__sun__) || defined(__sun)) && (defined(__svr4__) || defined(__SVR4))
-#  if defined(__sparc__) || defined(__sparc) 
-#    define SP_BIG_ENDIAN 
-#  else 
-#    define SP_LITTLE_ENDIAN 
-#  endif 
 #  define IMAGEDISPLAY 
 #  define NOSTRDUP 
 #endif 
@@ -117,7 +104,6 @@ extern "C" {
 typedef int mode_t;
 #define ftruncate(fd,len) _chsize(fd,len)
 #define sysconf(x) 512
-#define SP_LITTLE_ENDIAN
 #define NOPIPE
 #define NOLOCKF
 #define NOSTRCASECMP
@@ -130,7 +116,6 @@ typedef int mode_t;
  * Microsoft Windows running MinGW
  */
 #if defined(__MINGW32__)
-#define SP_LITTLE_ENDIAN
 /* #define mkdir(filename,mode) mkdir((filename)) */
 #define NOPIPE
 #define NOLOCKF
@@ -158,16 +143,6 @@ typedef int mode_t;
 #endif /* _WIN32 */
 
 /*
- * Linux on Intel platforms
- */
-#if defined(__linux__)
-#  if defined(SP_BIG_ENDIAN)
-#    undef SP_BIG_ENDIAN
-#  endif
-#  define SP_LITTLE_ENDIAN
-#endif
-
-/*
  * Linux on AMD64 also needs to use va_copy()
  */
 #if defined(__linux__) && defined(__amd64__)
@@ -180,19 +155,13 @@ typedef int mode_t;
  * DEC Alpha's running Digital UNIX
  */
 #if defined(__alpha)
-#  if defined(SP_BIG_ENDIAN)
-#    undef SP_BIG_ENDIAN
-#  endif
-#  if !defined(SP_LITTLE_ENDIAN)
-#    define SP_LITTLE_ENDIAN
-#  endif
+/* Nothing as yet */
 #endif
 
 /*
  * Silicon Graphics - Irix
  */
 #if defined(__sgi)
-#define SP_BIG_ENDIAN
 #define NOSTRDUP
 #define NO_STRPTIME
 #endif
@@ -201,15 +170,98 @@ typedef int mode_t;
  * Macs (<= OS 9) - yuk!
  */
 #if defined(MAC)
-#define SP_BIG_ENDIAN
 #define NOSTRDUP
 #endif
 
-#if defined(__APPLE__)
-#define SP_BIG_ENDIAN
+#if defined(__APPLE__) && defined(__ppc__)
 #define NO_STRPTIME
 #define NOLOCKF
 #endif
+
+#if defined(__APPLE__) && defined(__i386__)
+/* nothing untoward as yet */
+#endif
+
+/*
+ *-----------------------------------------------------------------------------
+ * Machine endianness.
+ * These are overridden at the end when using autoconf, but are here for
+ * when being built as part of the Staden Package.
+ *-----------------------------------------------------------------------------
+ */
+
+/*
+ * x86 equivalents
+ */
+#if defined(__i386__) || defined(__i386) || defined(__amd64__) || defined(__amd64)
+#  if defined(SP_BIG_ENDIAN)
+#    undef SP_BIG_ENDIAN
+#  endif
+#  define SP_LITTLE_ENDIAN
+#endif
+
+/*
+ * DEC Alpha
+ */
+#if defined(__alpha__) || defined(__alpha)
+#  if defined(SP_LITTLE_ENDIAN)
+#    undef SP_LITTLE_ENDIAN
+#  endif
+#  define SP_BIG_ENDIAN
+#endif
+
+/*
+ * SUN Sparc
+ */
+#if defined(__sparc__) || defined(__sparc)
+#  if defined(SP_LITTLE_ENDIAN)
+#    undef SP_LITTLE_ENDIAN
+#  endif
+#  define SP_BIG_ENDIAN
+#endif
+
+/*
+ * PowerPC
+ */
+#if defined(__ppc__) || defined(__ppc)
+#  if defined(SP_LITTLE_ENDIAN)
+#    undef SP_LITTLE_ENDIAN
+#  endif
+#  define SP_BIG_ENDIAN
+#endif
+
+/* Some catch-alls */
+#if defined(__LITTLE_ENDIAN__) || defined(__LITTLEENDIAN__)
+#    define SP_LITTLE_ENDIAN
+#endif
+
+#if defined(__BIG_ENDIAN__) || defined(__BIGENDIAN__)
+#    define SP_BIG_ENDIAN
+#endif
+
+/* autoconf */
+#ifdef HAVE_CONFIG_H
+#    if defined(WORDS_BIGENDIAN)
+#        ifdef SP_LITTLE_ENDIAN
+#            undef SP_LITTLE_ENDIAN
+#        endif
+#        define SP_BIG_ENDIAN
+#    else
+#        ifdef SP_BIG_ENDIAN
+#            undef SP_BIG_ENDIAN
+#        endif
+#        define SP_LITTLE_ENDIAN
+#    endif
+#endif
+
+#if defined(SP_BIG_ENDIAN) && defined(SP_LITTLE_ENDIAN)
+#    error Both BIG and LITTLE endian defined. Fix os.h and/or Makefile
+#endif
+
+#if !defined(SP_BIG_ENDIAN) && !defined(SP_LITTLE_ENDIAN)
+#    error Neither BIG nor LITTLE endian defined. Fix os.h and/or Makefile
+#endif
+
 
 /*
  *-----------------------------------------------------------------------------
@@ -384,18 +436,6 @@ typedef int4 int_fl;		/* f_implicit */
 #define le_int4(x) (x)
 #define le_int2(x) (x)
 #define le_int1(x) (x)
-#endif
-
-#ifndef SP_BIG_ENDIAN
-#ifndef SP_LITTLE_ENDIAN
-#error Must define SP_BIG_ENDIAN or SP_LITTLE_ENDIAN in Makefile
-#endif
-#endif
-
-#ifdef SP_BIG_ENDIAN
-#ifdef SP_LITTLE_ENDIAN
-#error Must only define one of SP_BIG_ENDIAN and SP_LITTLE_ENDIAN in Makefile
-#endif
 #endif
 
 #ifdef __cplusplus
