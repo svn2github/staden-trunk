fe41ced29c62d0c8f742b09e7ce43ff1ee11ba76
jkbonfield <jkbonfield@ba6f2710-ca9f-427c-89d3-5bf716dbc8e1>
2008-12-02 12:38:54 +0000

    Added avg_sequence_depth prototype.
    git-svn-id: https://staden.svn.sourceforge.net/svnroot/staden/staden/trunk@1388 ba6f2710-ca9f-427c-89d3-5bf716dbc8e1

--- src/gap5/read_depth.h
+++ src/gap5/read_depth.h
@@ -5,4 +5,19 @@ typedef struct {
     int min, max, avg;
 } min_max_avg_t;
 
+/*
+ * Computes the reading depth over a range within the contig. Note that 
+ * the resolution of data returned may not be at the per-base level
+ * depending on the size of the range queried. The actual resolution
+ * of the data is samples from start_out to end_out in steps of
+ * inc_out.
+ *
+ * The array is xmalloced() and it is up to the caller to xfree() it.
+ *
+ * Returns average sequence depth on success (plus first/res filled out)
+ *         NULL on failure (first/res undefined).
+ */
+int *avg_sequence_depth(GapIO *io, int cnum, int start, int end,
+			int *start_out, int *end_out, int *inc_out);
+
 #endif /* _READ_DEPTH_H */
