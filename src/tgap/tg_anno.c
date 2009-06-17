b394d55a553e895b0e55194f683593333ac4632b
jkbonfield <jkbonfield@ba6f2710-ca9f-427c-89d3-5bf716dbc8e1>
2009-06-17 11:27:17 +0000

    Added an anno_get_range() function to return the range_t struct associated with an annotation. This allows us to query positional data, flags, etc.
    git-svn-id: https://staden.svn.sourceforge.net/svnroot/staden/staden/trunk@1701 ba6f2710-ca9f-427c-89d3-5bf716dbc8e1

--- src/tgap/tg_anno.c
+++ src/tgap/tg_anno.c
@@ -26,3 +26,67 @@ int anno_ele_new(GapIO *io, int bin,
 
     return rec;
 }
+
+/*
+ * Returns the range_t element from the bin holding this annotation.
+ * The start and end have been modified to be the absolute position
+ * within the contig.
+ *
+ * Returns a static range_t pointer on success (valid until next call)
+ *         NULL on failure.
+ */
+range_t *anno_get_range(GapIO *io, int anno_ele, int *contig) {
+    anno_ele_t *e = (anno_ele_t *)cache_search(io, GT_AnnoEle, anno_ele);
+    bin_index_t *bin;
+    int bnum, offset1, offset2, comp = 0, i;
+    range_t *r;
+    static range_t r2;
+
+    if (!e)
+	return NULL;
+
+    /* Find and load the associated bin */
+    bnum = e->bin;
+    bin = (bin_index_t *)cache_search(io, GT_Bin, bnum);
+    if (!bin->rng)
+	return NULL;
+
+    /* Find the appropriate range_t element */
+    for (i = 0; i < ArrayMax(bin->rng); i++) {
+	r = arrp(range_t, bin->rng, i);
+	if (r->rec == anno_ele)
+	    break;
+    }
+    if (r->rec != anno_ele)
+	return NULL;
+
+    /* Find the position and orientation of this bin relative to the contig */
+    offset1 = r->start;
+    offset2 = r->end;
+    for (;;) {
+        if (bin->flags & BIN_COMPLEMENTED) {
+            offset1 = bin->size-1 - offset1;
+            offset2 = bin->size-1 - offset2;
+            comp ^= 1;
+        }
+        offset1 += bin->pos;
+        offset2 += bin->pos;
+
+        if (bin->parent_type != GT_Bin)
+            break;
+
+        bnum = bin->parent;
+        bin = (bin_index_t *)cache_search(io, GT_Bin, bnum);
+    }
+
+    assert(bin->parent_type == GT_Contig);
+
+    r2 = *r;
+    r2.start = offset1;
+    r2.end   = offset2;
+
+    if (contig)
+	*contig = bin->parent;
+
+    return &r2;
+}
--- src/tgap/tg_anno.h
+++ src/tgap/tg_anno.h
@@ -10,6 +10,16 @@ int anno_ele_new(GapIO *io, int bin,
 		 int obj_type, int obj_rec, int anno_rec,
 		 int type, char *comment);
 
+/*
+ * Returns the range_t element from the bin holding this annotation.
+ * The start and end have been modified to be the absolute position
+ * within the contig.
+ *
+ * Returns a static range_t pointer on success (valid until next call)
+ *         NULL on failure.
+ */
+range_t *anno_get_range(GapIO *io, int anno_ele, int *contig);
+
 #define str2type(s) ((s)[3] + ((s)[2]<<8) + ((s)[1]<<16) + ((s)[0]<<24))
 #define type2str(t,s) \
     ( \
