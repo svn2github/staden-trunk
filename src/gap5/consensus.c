353436437b5159b9ad947e398b6e12ab20dd62e0
jkbonfield <jkbonfield@ba6f2710-ca9f-427c-89d3-5bf716dbc8e1>
2009-09-10 15:20:09 +0000

    Added code (currently "#if 0"ed out) to attempt to cache the consensus in tracks per bin. Specifically the plan was to store the A,C,G,T,* accumulated log(probability) values for data held within that bin as a track. It means we can compute the consensus without having to load all the invdividual sequences.
    In practice, it does speed up the consensus generation when cached
    (usually only barely, but occasionally by > 10x), but the downsides
    are considerable. It needs read-write access, and worst of all the
    space it takes up is very substantial - often far more than the rest
    of the database. The existing implementation also is only approximate
    as we store the floating point values in very low precision.
    
    Given the size problems I'm planning on reimplementing this via
    another means, but the code is checked in here (disabled) so I can
    revert or refer back to it at some stage if required.
    
    Also see the code changes to tg_iface_g.c.
    
    
    git-svn-id: https://staden.svn.sourceforge.net/svnroot/staden/staden/trunk@1832 ba6f2710-ca9f-427c-89d3-5bf716dbc8e1

--- src/gap5/consensus.c
+++ src/gap5/consensus.c
@@ -3,6 +3,7 @@
 #include <ctype.h>
 #include <string.h>
 #include <float.h>
+#include <assert.h>
 
 /* See consensus.txt for discussions on these algorithms */
 
@@ -11,6 +12,9 @@
 #include "consensus.h"
 
 #define CONS_BLOCK_SIZE 1024
+
+#define LOG10        2.30258509299404568401
+#define TENOVERLOG10 4.34294481903251827652
 
 /*
  * Empirically derived constants for various sequencing technologies, with
@@ -124,6 +128,207 @@ int calculate_consensus(GapIO *io, int contig, int start, int end,
     return 0;
 }
 
+
+typedef struct {
+    double base_qual[4];
+    double gap;
+    double base;
+    int depth;
+} cstat;
+
+/*
+ * Like contig_seqs_in_range, but we also sum the values into the cvec array
+ * at the same time.
+ *
+ * The purpose of this is to allow partial or complete caching of the
+ * consensus data, meaning we only need to load bin tracks instead of
+ * all the sequences themselves.
+ */
+#define NORM(x) (f_a * (x) + f_b)
+#define NMIN(x,y) (MIN(NORM((x)),NORM((y))))
+#define NMAX(x,y) (MAX(NORM((x)),NORM((y))))
+static int contig_consensus_in_range2(GapIO *io, int brec, int start, int end,
+				      int offset, cstat *cv, int complement) {
+    double slx_overcall_prob  = 1.0/4350000, lover,  lomover;
+    double slx_undercall_prob = 1.0/2800000, lunder, lomunder;
+    int i, n, f_a, f_b;
+    bin_index_t *bin = get_bin(io, brec);
+    range_t *l;
+
+    /* log overcall, log one minus overcall, etc */
+    lover    = log(slx_overcall_prob);
+    lomover  = log(1-slx_overcall_prob);
+    lunder   = log(slx_undercall_prob);
+    lomunder = log(1-slx_undercall_prob);
+
+    cache_incr(io, bin);
+    if (bin->flags & BIN_COMPLEMENTED) {
+	complement ^= 1;
+    }
+
+    if (complement) {
+	f_a = -1;
+	f_b = offset + bin->size-1;
+    } else {
+	f_a = +1;
+	f_b = offset;
+    }
+
+    /*
+    printf("BIN #%d %d + %d..%d (%d..%d)\n",
+	   brec, offset, bin->pos, bin->pos + bin->size,
+	   bin->start_used, bin->end_used);
+    */
+    if (!(end < NMIN(bin->start_used, bin->end_used) ||
+	  start > NMAX(bin->start_used, bin->end_used))
+	&& bin->rng) {
+	track_t *tr = NULL;
+	cstat *loc;
+
+	tr = bin_get_track(io, bin, TRACK_CONS_ARR);
+
+	if (tr) {
+	    loc = arrp(cstat, tr->data, 0);
+	} else {
+	    tr = bin_create_track(io, bin, TRACK_CONS_ARR);
+	    //tr = track_create_fake(TRACK_CONS_ARR, 0);
+	    tr->nitems    = bin->end_used - bin->start_used + 1;
+	    tr->item_size = sizeof(cstat);
+	    tr->data      = ArrayCreate(tr->item_size, tr->nitems);
+	    //tr->flag     |= TRACK_FLAG_FREEME;
+
+	    bin_add_track(io, &bin, tr);
+    
+	    loc = arrp(cstat, tr->data, 0);
+	    memset(&loc[0], 0, (bin->end_used - bin->start_used + 1) *
+		   sizeof(*loc));
+
+	    for (n = 0; n < ArrayMax(bin->rng); n++) {
+		l = arrp(range_t, bin->rng, n);
+
+		if (l->flags & (GRANGE_FLAG_UNUSED | GRANGE_FLAG_ISANNO))
+		    continue;
+
+		if (1 || (NMAX(l->start, l->end) >= start
+			  && NMIN(l->start, l->end) <= end)) {
+		    seq_t *s = (seq_t *)cache_search(io, GT_Seq, l->rec);
+		    seq_t *sorig = s;
+		    int p;
+
+		    if ((s->len < 0)) {
+			s = dup_seq(s);
+			complement_seq_t(s);
+		    }
+
+		    /*
+		      printf("Seq %c%d %d..%d\n",
+		      s == sorig ? '>' : '<',
+		      l->rec, s->left, s->right);
+		    */
+		    for (p = s->left-1; p < s->right; p++) {
+			char base;
+			double q[4];
+			int j, k;
+
+			sequence_get_base4(io, &s, p, &base, q, 0);
+			/*
+			  printf("  %5d+%3d %2d %c %f %f %f %f\n",
+			  offset, p + l->start, p,
+			  base, q[0], q[1], q[2], q[3]);
+			*/
+		    
+			k = p + l->start - bin->start_used;
+			if (k < 0 || k >= bin->end_used - bin->start_used + 1) {
+			    printf("Error: start/end used summary is invalid\n");
+			}
+			assert(k >= 0 &&  k < bin->end_used - bin->start_used + 1);
+
+			switch (lookup[base]) {
+			case 0: case 1: case 2: case 3: /* ACGT */
+			    loc[k].base_qual[0] += q[0];
+			    loc[k].base_qual[1] += q[1];
+			    loc[k].base_qual[2] += q[2];
+			    loc[k].base_qual[3] += q[3];
+
+			    /* Small boost for called base to resolve ties */
+			    loc[k].base_qual[lookup[base]] += 1e-5;
+
+			    /* Fall through */
+			default: /* N */
+			    loc[k].gap  += lover;
+			    loc[k].base += lomover;
+			    break;
+
+			case 4: /* gap */
+			    loc[k].gap  += lomunder;
+			    loc[k].base += lunder;
+			    break;
+			}
+
+			loc[k].depth++;
+		    }
+
+		    if (s != sorig)
+			free(s);
+		}
+	    }
+	}
+	
+
+	/*
+	printf("Bin %d summary\n", brec);
+	for (i = 0; i <= bin->end_used - bin->start_used; i++) {
+	    printf("    %4d %4d %2d %f %f %f %f\n",
+		   i + bin->start_used,
+		   loc[i].depth,
+		   loc[i].base_qual[0],
+		   loc[i].base_qual[1],
+		   loc[i].base_qual[2],
+		   loc[i].base_qual[3]);
+	}
+	*/
+	
+	for (i = 0; i <= bin->end_used - bin->start_used; i++) {
+	    int j = NORM(i + bin->start_used);
+	    if (j >= start && j <= end) {
+		int k = j-start;
+		cv[k].base_qual[0] += loc[i].base_qual[0];
+		cv[k].base_qual[1] += loc[i].base_qual[1];
+		cv[k].base_qual[2] += loc[i].base_qual[2];
+		cv[k].base_qual[3] += loc[i].base_qual[3];
+		cv[k].gap          += loc[i].gap;
+		cv[k].base         += loc[i].base;
+		cv[k].depth        += loc[i].depth;
+	    }
+	}
+	
+	//free(loc);
+    }
+
+    /* Recurse down bins */
+    for (i = 0; i < 2 > 0; i++) {
+	bin_index_t *ch;
+	if (!bin->child[i])
+	    continue;
+	ch = get_bin(io, bin->child[i]);
+	if (end   >= NMIN(ch->pos, ch->pos + ch->size-1) &&
+	    start <= NMAX(ch->pos, ch->pos + ch->size-1)) {
+	    contig_consensus_in_range2(io, bin->child[i], start, end,
+				       NMIN(ch->pos, ch->pos + ch->size-1),
+				       cv, complement);
+	}
+    }
+
+    cache_decr(io, bin);
+    return 0;
+}
+
+int contig_consensus_in_range(GapIO *io, contig_t **c, int start, int end,
+			      cstat *cv) {
+    return contig_consensus_in_range2(io, contig_get_bin(c), start, end,
+				      contig_offset(io, c), cv, 0);
+}
+
 /*
  * The core of the consensus algorithm.
  *
@@ -138,7 +343,7 @@ int calculate_consensus(GapIO *io, int contig, int start, int end,
 static int calculate_consensus_bit(GapIO *io, int contig, int start, int end,
 				   consensus_t *cons) {
     int i, j, nr;
-    rangec_t *r;
+    rangec_t *r = NULL;
     contig_t *c = (contig_t *)cache_search(io, GT_Contig, contig);
     int len = end - start + 1;
     double slx_overcall_prob  = 1.0/4350000, lover,  lomover;
@@ -147,6 +352,7 @@ static int calculate_consensus_bit(GapIO *io, int contig, int start, int end,
     double (*cvec)[4]; /* cvec[0-3] = A,C,G,T */
     double (*pvec)[2]; /* pvec[0] = gap, pvec[1] = base */
     int *depth;
+    cstat *cs;
 
     /* log overcall, log one minus overcall, etc */
     lover    = log(slx_overcall_prob);
@@ -179,14 +385,34 @@ static int calculate_consensus_bit(GapIO *io, int contig, int start, int end,
 	    double p = 1 / (1 + pow(10, -i / 10.0));
 	    lo2l[i] = log(p);
 	    lo2r[i] = log((1-p)/3);
-	    lo2ph[i] = 10*log(1+pow(10, i/10.0))/log(10)+0.4999;
+	    lo2ph[i] = 10*log(1+pow(10, i/10.0))/LOG10+0.4999;
 	}
     }
+
+#if 0
+    /* Accumulate... (computes the products via sum of logs) */
+    /* This bit may be cached */
+    cs = (cstat *)malloc((end-start+1) * sizeof(*cs));
+    memset(&cs[0], 0, (end-start+1) * sizeof(*cs));
+    contig_consensus_in_range(io, &c, start, end, cs);
+
+    for (i = 0; i < end-start+1; i++) {
+	cvec[i][0] = cs[i].base_qual[0];
+	cvec[i][1] = cs[i].base_qual[1];
+	cvec[i][2] = cs[i].base_qual[2];
+	cvec[i][3] = cs[i].base_qual[3];
+	pvec[i][0] = cs[i].gap;
+	pvec[i][1] = cs[i].base;
+	depth[i]   = cs[i].depth;
+    }
+
+    free(cs);
+
+#else
 
     /* Find sequences visible */
     r = contig_seqs_in_range(io, &c, start, end, 0, &nr);
 
-    /* Accumulate... (computes the products via sum of logs) */
     for (i = 0; i < nr; i++) {
 	int sp = r[i].start;
 	seq_t *s = (seq_t *)cache_search(io, GT_Seq, r[i].rec);
@@ -257,6 +483,7 @@ static int calculate_consensus_bit(GapIO *io, int contig, int start, int end,
 	if (s != sorig)
 	    free(s);
     }
+#endif
 
 
     /* and speculate */
@@ -331,7 +558,7 @@ static int calculate_consensus_bit(GapIO *io, int contig, int start, int end,
 	 */
 	cons[i].scores[5] = 0;
 	for (j = 0; j < 4; j++) {
-	    cons[i].scores[j] = 10 / log(10) *
+	    cons[i].scores[j] = TENOVERLOG10 *
 		(log(base_prob * probs[j]) - log(tot2[j]));
 	}
 
@@ -395,7 +622,7 @@ int consensus_valid_range(GapIO *io, int contig, int *start, int *end) {
 		if (cons[i].depth)
 		    break;
 	    }
-	} while (i == CONS_SZ);
+	} while (i == CONS_SZ && pos <= c->end);
 
 	*start = pos;
     }
@@ -410,7 +637,7 @@ int consensus_valid_range(GapIO *io, int contig, int *start, int *end) {
 		if (cons[i].depth)
 		    break;
 	    }
-	} while (i == 0);
+	} while (i == 0 && pos >= c->start);
 
 	*end = pos;
     }
