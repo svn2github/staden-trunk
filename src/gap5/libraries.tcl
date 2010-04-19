d27aa4173e6cc6f73b6d9549bde52988f6af082f
jkbonfield <jkbonfield@ba6f2710-ca9f-427c-89d3-5bf716dbc8e1>
2010-04-19 10:23:15 +0000

    Updated the List Libraries window to use Plotchart. It now has axis and things, but needs more work still.
    git-svn-id: https://staden.svn.sourceforge.net/svnroot/staden/staden/trunk@2097 ba6f2710-ca9f-427c-89d3-5bf716dbc8e1

--- src/gap5/libraries.tcl
+++ src/gap5/libraries.tcl
@@ -1,36 +1,4 @@
-proc ListLibraries {io} {
-    set t [xtoplevel .list_libraries]
-    if {$t == ""} return
-    wm title $t "List Libraries"
-    wm geometry $t 550x200
-
-    # Create our tablelist
-    tablelist $t.list \
-	-columns {7 Index 15 "Name" 10 "Pair count" 8 "Type" 10 "Insert size" 6 "s.d." 15 "Orientation"} \
-        -labelcommand tablelist::sortByColumn \
-	-exportselection 0 \
-	-stretch 0 \
-        -yscrollcommand [list $t.yscroll set]
-
-    $t.list columnconfigure 0 -sortmode integer
-    $t.list columnconfigure 2 -sortmode integer
-    $t.list columnconfigure 4 -sortmode integer
-    $t.list columnconfigure 5 -sortmode real
-    
-    # Add scrollbars
-    scrollbar $t.yscroll -command "$t.list yview"
-
-    # Layout
-    grid columnconfigure $t 0 -weight 1
-    grid rowconfigure $t 0 -weight 1
-    grid $t.list $t.yscroll -sticky nsew
-
-    # Add some bindings
-    bind [$t.list bodypath] <<use>> "+ListLibrariesDetailed $io %W $t.list %x %y"
-
-    # Populate the list
-    ListLibrariesPopulate $io $t.list
-}
+package require Plotchart
 
 ;proc ListLibrariesPopulate {io w} {
     # Clear any existing data
@@ -75,13 +43,54 @@ proc ListLibraries {io} {
     #$db delete
 }
 
+proc ListLibraries {io} {
+    set t [xtoplevel .list_libraries]
+    if {$t == ""} return
+    wm title $t "List Libraries"
+    wm geometry $t 550x600
+
+    # Create our tablelist
+    tablelist $t.list \
+	-columns {7 Index 15 "Name" 10 "Pair count" 8 "Type" 10 "Insert size" 6 "s.d." 15 "Orientation"} \
+        -labelcommand tablelist::sortByColumn \
+	-exportselection 0 \
+	-stretch 0 \
+        -yscrollcommand [list $t.yscroll set]
+
+    $t.list columnconfigure 0 -sortmode integer
+    $t.list columnconfigure 2 -sortmode integer
+    $t.list columnconfigure 4 -sortmode integer
+    $t.list columnconfigure 5 -sortmode real
+    
+    # Add scrollbars
+    scrollbar $t.yscroll -command "$t.list yview"
+
+    # Canvas
+    canvas $t.c
+
+    # Layout
+    grid columnconfigure $t 0 -weight 1
+    grid rowconfigure $t 0 -weight 1
+    grid rowconfigure $t 1 -weight 1
+    grid $t.list $t.yscroll -sticky nsew
+    grid $t.c -sticky nsew -columnspan 2
+
+    # Add some bindings
+#    bind [$t.list bodypath] <<use>> "+ListLibrariesDetailed $io %W $t.list %x %y"
+    bind [$t.list bodypath] <<select>> "+ListLibrariesDetailed $io %W $t %x %y"
+
+    # Populate the list
+    ListLibrariesPopulate $io $t.list
+}
+
+
 # Double-click callback from the library list.
 ;proc ListLibrariesDetailed {io w t x y} {
     # Move x,y to parent tablelist coordinates
     incr y [lindex [split [winfo geometry $w] +] 2]
     incr x [lindex [split [winfo geometry $w] +] 1]
 
-    set row [$t containing $y]
+    set row [$t.list containing $y]
     if {$row == -1} return
 
     incr row
@@ -89,33 +98,37 @@ proc ListLibraries {io} {
     set db [$io get_database]
     set rec [$db get_library_rec $idx]
 
-    draw_lib $io $t.lib_$rec $rec
+    draw_lib $io $t.c $rec
 }
 
 # Detailed data about a library including the distribution graph
-;proc draw_lib {io w rec} {
-    if {[xtoplevel $w] == ""} return
+;proc draw_lib {io c rec} {
+    if {![winfo exists $c]} {
+	#if {[xtoplevel $w] == ""} return
+	scrollbar $w.xs -orient horiz -command "$c xview"
+	scrollbar $w.ys -orient vert  -command "$c yview"
+	canvas $c \
+	    -xscrollcommand "$w.xs set" \
+	    -yscrollcommand "$w.ys set" \
+	    -height 400 \
+	    -width 600
+	grid rowconfigure    $w 0 -weight 1 -minsize 0
+	grid columnconfigure $w 0 -weight 1 -minsize 0
+	grid $c    -row 0 -column 0 -sticky nsew
+	grid $w.xs -row 1 -column 0 -sticky nsew
+	grid $w.ys -row 0 -column 1 -sticky nsew
+    }
 
+    # Get coordinate list
     set lib [$io get_library $rec]
     set dist [$lib get_dist]
 
-    set c $w.c
-    scrollbar $w.xs -orient horiz -command "$c xview"
-    scrollbar $w.ys -orient vert  -command "$c yview"
-    canvas $c \
-	-xscrollcommand "$w.xs set" \
-	-yscrollcommand "$w.ys set" \
-	-height 400 \
-	-width 600
-    grid rowconfigure    $w 0 -weight 1 -minsize 0
-    grid columnconfigure $w 0 -weight 1 -minsize 0
-    grid $c    -row 0 -column 0 -sticky nsew
-    grid $w.xs -row 1 -column 0 -sticky nsew
-    grid $w.ys -row 0 -column 1 -sticky nsew
-
-    # Create the plots
+    set l_0 {}
+    set l_1 {}
+    set l_2 {}
     set maxy 1
     set maxx 1
+    set x_at_maxy 1
     for {set j 0} {$j < 3} {incr j} {
 	set last_x 0
 	set last_y 0
@@ -123,53 +136,49 @@ proc ListLibraries {io} {
 	foreach {x y} [lindex $dist $j] {
 	    if {$last_x == 0} {set last_x $x; continue}
 	    #set y [expr {log($y+1)}]
-	    $c create line $last_x $last_y $x $y -tags type_$j -fill $col -width 3
+	    lappend l_$j $x $y
+	    #$c create line $last_x $last_y $x $y -tags type_$j -fill $col -width 3
 	    set last_x $x
 	    set last_y $y
-	    if {$maxy < $y} {set maxy $y}
+	    if {$maxy < $y} {set maxy $y; set x_at_maxy $x}
 	    if {$maxx < $x} {set maxx $x}
 	}
     }
 
-    $c scale all 0 0 1 -1
-    $c move all 0 $maxy
-    $c scale all 0 0 1 [expr {400.0/$maxy}]
+    set bsize [expr {int(log($x_at_maxy)/log(2)-7)}]
+    if {$bsize < 1} {set bsize 1}
+    set maxy [expr {$maxy * $bsize*1.05}]
 
-    # Identify an *appropriate* scrollregion.
-    set y2 0
-    for {set x $maxx} {$x >= 100} {set x [expr {int($x * 0.9)}]} {
-	set l [$c find enclosed $x -10000 10000000 10000]
-	if {$l != {}} {
-	    foreach {x1 y1 x2 y2} [eval $c bbox $l] break;
-	    if {[expr {abs($y1-$y2)}] >= 10} break
+    # Find tune X range
+    set minx $maxx
+    set maxx 1
+    set y100 [expr {$maxy / 100}]
+    foreach {x y} "$l_0 $l_1 $l_2" {
+	if {$y >= $y100} {
+	    if {$x < $minx} {set minx $x}
+	    if {$x > $maxx} {set maxx $x}
 	}
     }
-    set maxx [expr {int($x/0.9)}]
-    set maxy $y2
-    
-    # Create the ruler
-    for {set x 0} {$x < $maxx+100} {incr x 10} {
-	if {[expr {$x%1000}] == 0} {
-	    set h 40
-	} elseif {[expr {$x%100}] == 0} {
-	    set h 20
-	} else {
-	    set h 10
-	}
-	$c create line $x 0 $x $h -tags ruler
-    }
 
-    # Plot Labels
-    set o 40
+    # Create the plots
+    incr minx -20
+    if {$minx < 1} {set minx 1}
+    incr maxx 20
+
+    foreach {xn xi} [nice_num $minx [expr {$maxx-$minx+1}] 0] {break}
+    foreach {yn yi} [nice_num 0 $maxy 0] {break}
+
+    $c delete all
+    set s [::Plotchart::createXYPlot $c \
+	       [list $xn $maxx $xi] \
+	       [list 0 $maxy $yi]]
+
+    # Plot it
     for {set j 0} {$j < 3} {incr j} {
-	set col [lindex {black orange3 blue} $j]
-	set dir [lindex {"--->  <---" "<---  --->" "<---  <---\n--->  --->"} $j]
-	if {$j == 2} {incr o 5}
-	$c create text 25 [expr {$j*20+$o}] -text $dir -fill $col -anchor w
-	$c create line 5 [expr {$j*20+$o}] 20 [expr {$j*20+$o}] -fill $col -width 3
+	foreach {x y} [set l_$j] {
+	    $s plot lib_${rec}_$j $x [expr {$y*$bsize}]
+	}
     }
-
-    $c configure -scrollregion "0 0 $maxx $maxy"
 
     $lib delete
 }
