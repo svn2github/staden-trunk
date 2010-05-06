ee2cd640db7a94a21f42afe6aaeb1f2bdc6da212
jkbonfield <jkbonfield@ba6f2710-ca9f-427c-89d3-5bf716dbc8e1>
2010-05-06 09:28:20 +0000

    First draft of oligo selection code within the editor.
    git-svn-id: https://staden.svn.sourceforge.net/svnroot/staden/staden/trunk@2114 ba6f2710-ca9f-427c-89d3-5bf716dbc8e1

---
+++ src/gap5/editor_oligo.c
@@ -0,0 +1,153 @@
+/*
+ * The "select_oligos" function of the contig editor.
+ *
+ * FIXME: move this outside of the editor and use it as a general
+ * command operating on a contig region? (Or contigs - for PCR.)
+ */
+
+#include <tcl.h>
+
+#include "editor_view.h"
+#include "primlib.h"
+
+/*
+ *----------------------------------------------------------------------------
+ * Publically callable functions below here
+ *----------------------------------------------------------------------------
+ */
+
+/*
+ * Find suitable oligos using Primer3 with current parameter settings.
+ * Return a Tcl list object on success.
+ *          NULL on failure.
+number of oligos found (or -1 for error).
+ */
+Tcl_Obj *edSelectOligoGenerate(edview *xx, int is_fwds, int bkwd_width,
+			       int fwd_width, int avg_read_len,
+			       char *primer_defs) {
+    Tcl_Obj *lobj;
+    int seq  = xx->cursor_rec;
+    int pos  = xx->cursor_pos;
+    int apos = xx->cursor_apos;
+
+    int i, j;
+    primlib_state *state;
+    primlib_args *args;
+
+    char *consensus;
+    int *opos;
+    int left, right;
+    int cleft, cright;
+    int consensusLength;
+
+
+    /* Create an initialise primlib state */
+    state = primlib_create();
+    if (NULL == (args = primlib_str2args(primer_defs)))
+	return NULL;
+
+    primlib_set_args(state, args);
+    free(args);
+
+    /*
+     * Conceptually we select off consensus.
+     * Determine consensus around point...
+     *    the oligo will be selected from this region
+     */
+    if (is_fwds) {
+	left  = apos - bkwd_width;
+	right = apos + fwd_width;
+    } else {
+	left  = apos - fwd_width;
+	right = apos + bkwd_width;
+    }
+    if (0 == consensus_valid_range(xx->io, xx->cnum, &cleft, &cright)) {
+	if (left < cleft)
+	    left = cleft;
+	if (right > cright)
+	    right = cright;
+    } else {
+	if (left < xx->contig->start)
+	    left = xx->contig->start;
+	if (right > xx->contig->end)
+	    right = xx->contig->end;
+    }
+
+    /*
+     * Allocated and calculate the consensus sequence.
+     */
+    consensusLength = right - left + 1;
+    if (NULL == (consensus = (char *)xmalloc(consensusLength+1)) ||
+	NULL == (opos = (int *)xmalloc((consensusLength+1)*sizeof(int)))) {
+	bell();
+	return NULL;
+    }
+    calculate_consensus_simple(xx->io, xx->cnum, left, right, consensus, NULL);
+    consensus[consensusLength] = 0;
+
+    /*
+     * Complement if necessary
+     */
+    if (!is_fwds) {
+	complement_seq(consensus, consensusLength);
+    }
+
+    /* Depad it */
+    for (j = i = 0; i < consensusLength; i++) {
+	opos[i] = j;
+	if (consensus[i] != '*') {
+	    consensus[j] = consensus[i];
+	    j++;
+	}
+    }
+    consensus[j] = 0;
+
+    /* Pick the actual oligos */
+    if (-1 == primlib_choose(state, consensus) || state->nprimers == 0) {
+	xfree(opos);
+	xfree(consensus);
+	primlib_destroy(state);
+
+	return NULL;
+    }
+
+
+    /*
+     * Generate a Tcl list of the results.
+     *
+     * This consists of start, end, seq, quality, gc_content, melting temp.
+     */
+    lobj = Tcl_NewListObj(0, NULL);
+    for (i = 0; i < state->nprimers; i++) {
+	Tcl_Obj *l = Tcl_NewListObj(0, NULL);
+	int st = state->primers[i].start, st2 = st;
+	int en = st + state->primers[i].length-1, en2 = en;
+	for (j = st; j < consensusLength; j++) {
+	    if (opos[j] == st)
+		st2 = j;
+	    if (opos[j] == en)
+		en2 = j;
+	}
+
+	Tcl_ListObjAppendElement(xx->interp, l,
+				 Tcl_NewIntObj(st2+left));
+	Tcl_ListObjAppendElement(xx->interp, l,
+				 Tcl_NewIntObj(en2+left));
+	Tcl_ListObjAppendElement(xx->interp, l,
+				 Tcl_NewStringObj(&consensus[st], en-st+1));
+	Tcl_ListObjAppendElement(xx->interp, l,
+				 Tcl_NewDoubleObj(state->primers[i].quality));
+	Tcl_ListObjAppendElement(xx->interp, l,
+				 Tcl_NewDoubleObj(state->primers[i].gc_content));
+	Tcl_ListObjAppendElement(xx->interp, l,
+				 Tcl_NewDoubleObj(state->primers[i].temp));
+	Tcl_ListObjAppendElement(xx->interp, lobj, l);
+    }
+
+    xfree(opos);
+    xfree(consensus);
+    primlib_destroy(state);
+
+    return lobj;
+}
+
---
+++ src/gap5/editor_oligo.h
@@ -0,0 +1,66 @@
+/*
+ * File: oligo.h
+ * Version:
+ *
+ * Author:
+ *         MRC Laboratory of Molecular Biology
+ *	   Hills Road
+ *	   Cambridge CB2 2QH
+ *	   United Kingdom
+ *
+ * Description:
+ *
+ * Created:
+ * Updated:
+ *
+ */
+
+#ifndef _EDITOR_OLIGO_H_
+#define _EDITOR_OLIGO_H_
+
+#include "editor_view.h"
+
+/*
+ * Find suitable oligos using Primlib with current parameter settings.
+ * Return number of oligos found (or -1 for error).
+ */
+int edSelectOligoGenerate(edview *xx, int sense, int bkwd_width,
+			  int fwd_width, int avg_read_len, char *pdefs);
+
+/*
+ * We cycle through the oligo list
+ * curr-oligo gives the current oligo entry under consideration.
+ *
+ * We return a list of strings in the form:
+ * default_tname tname ...
+ *
+ * The string returned is malloced and so is expected to be freed by the
+ * calling routine.
+ */
+char *edSelectOligoNext(edview *xx);
+char *edSelectOligoPrev(edview *xx);
+
+/*
+ * Select the current oligo.
+ * Creats a tag for it and everything else...
+ *
+ * Returns a status line containing the template and sequence
+ */
+char *edSelectOligoAccept(edview *xx, char *template_name);
+
+/*
+ * Frees up the temporary structures used by the oligo selection code.
+ */
+void edSelectOligoQuit(edview *xx);
+
+/*
+ * Creates a temporary annotation view, much like the suggest primers and the
+ * pick pcr primer functions.
+ * It bypasses the undo mechanism and will not be saved.
+ *
+ * NB: Currently this ignores many of the arguments.
+ */
+void createTmpAnnotation(edview *xx, int seq, int pos, int len,
+			 char *type, char *text, int strand);
+
+#endif /* _EDITOR_OLIGO_H_ */
