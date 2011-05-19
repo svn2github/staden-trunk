81c5d3abed3e8bdc26feba3f89d8601ddc693688
awhitwham <awhitwham@ba6f2710-ca9f-427c-89d3-5bf716dbc8e1>
2011-05-19 14:14:42 +0000

    Added missing header file.
    git-svn-id: https://staden.svn.sourceforge.net/svnroot/staden/staden/trunk@2508 ba6f2710-ca9f-427c-89d3-5bf716dbc8e1

---
+++ src/gap5/afg.h
@@ -0,0 +1,15 @@
+/*
+ * afg.h - afg to gap5 conversion for tg_index
+ *
+ * Andrew Whitwham, January 2011
+ *
+ */
+ 
+#ifndef _AFG_H
+#define _AFG_H
+
+#include <tg_index.h>
+
+int parse_afg(GapIO *io, char *fn, tg_args *a);
+
+#endif  
